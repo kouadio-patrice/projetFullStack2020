29e219b3d9363d0ee20f3cffdea24d0b14f461d2
kouadio-patrice <koukpatrice@gmailcom>
2021-01-14 22:55:28 +0100

    course information, step1

---
+++ partie1_a/src/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+ 
+const Header = (props) => {
+
+  return(
+    <div>
+        <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = () =>{
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return(
+    <div>
+       <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+     
+    </div>
+  )
+ }
+    
+ const Total = (props) =>{
+ 
+   return(
+     <div>
+        <p>Number of exercise: {props.total}</p>
+      
+     </div>
+   )
+ }
+
+const App = () => {
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  const total = exercises1 + exercises2 +exercises3 ;
+
+   const course = 'Half Stack application development'
+
+  return (
+    <div>
+      <Header course = {course}/>
+        <Content />
+        <Total total ={total} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
